93e304c9a6a1c2bdc3766e5dc82cc4a0c5615a28
bhaskarayh <bhaskarayh@gmail.com>
2023-11-29 11:52:51 +0700

    add 9. rendering list

--- src/Pages/producs.jsx
+++ src/Pages/producs.jsx
@@ -1,27 +1,37 @@
 import CardProduct from '../components/Fragments/CardProduct';
 
+const products = [
+    {
+        id: 1,
+        name: 'Sepatu Baru',
+        price: 'Rp 1.000.000',
+        image: '/images/shoes.jpg',
+        desciption:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio nisi quaerat aliquam optio eos laborum expedita est deserunt quas repellat.',
+    },
+    {
+        id: 2,
+        name: 'Sepatu Lama',
+        price: 'Rp 500.000',
+        image: '/images/shoes.jpg',
+        desciption:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio nisi.',
+    },
+];
+
 const ProducsPage = () => {
     // console.log({ CardProduct });
     return (
-        <div className="flex justify-center py-5 gap-2.5">
-            <CardProduct>
-                <CardProduct.Header image="images/shoes.jpg" />
-                <CardProduct.Body title="Sepatu Baru">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Distinctio nisi quaerat aliquam optio eos laborum expedita
-                    est deserunt quas repellat.
-                </CardProduct.Body>
-                <CardProduct.Footer price="Rp 1.000.000" />
-            </CardProduct>
-            <CardProduct>
-                <CardProduct.Header image="images/shoes.jpg" />
-                <CardProduct.Body title="Sepatu Baru">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Distinctio nisi quaerat aliquam optio eos laborum expedita
-                    est deserunt quas repellat.
-                </CardProduct.Body>
-                <CardProduct.Footer price="Rp 1.000.000" />
-            </CardProduct>
+        <div className="flex flex-wrap justify-center py-5 gap-2.5">
+            {products.map(product => (
+                <CardProduct key={product.id}>
+                    <CardProduct.Header image={product.image} />
+                    <CardProduct.Body name={product.name}>
+                        {product.desciption}
+                    </CardProduct.Body>
+                    <CardProduct.Footer price={product.price} />
+                </CardProduct>
+            ))}
         </div>
     );
 };
