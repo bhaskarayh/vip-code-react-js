9bebc1cfaadca2038e4630b56a4db954b1ae177e
bhaskarayh <bhaskarayh@gmail.com>
2023-12-21 13:51:55 +0700

    13.Lifecycle

--- src/Pages/welcome.jsx
+++ src/Pages/welcome.jsx
@@ -1,9 +1,14 @@
+import Counter from '../components/Fragments/Counter';
+
 const Welcome = () => {
-  return (
-    <div className="flex justify-center">
-      <h1 className="text-3xl font-bold">Hello World!</h1>
-    </div>
-  );
+    return (
+        <div className="flex justify-center flex-col">
+            <h1 className="text-3xl font-bold text-center">Hello World!</h1>
+            <div className="mt-5">
+                <Counter></Counter>
+            </div>
+        </div>
+    );
 };
 
 export default Welcome;
--- src/components/Fragments/Counter.jsx
+++ src/components/Fragments/Counter.jsx
@@ -6,6 +6,24 @@ class Counter extends React.Component {
         this.state = {
             count: 0,
         };
+        // #1
+        console.log('Constructor');
+    }
+
+    // Melihat komponen pertama kali terjadi
+    /* Cek Diagramnya https://projects.wojtekmaj.pl/react-lifecycle-methods-diagram/ */
+    componentDidMount() {
+        this.setState({ count: 1 });
+        // #2
+        console.log('componentDidMount');
+    }
+
+    // Melihat setiap perubahan yang terjadi
+    componentDidUpdate() {
+        console.log('componentDidUpdate');
+        if (this.state.count === 10) {
+            this.setState({ count: 0 });
+        }
     }
 
     render() {
@@ -13,7 +31,7 @@ class Counter extends React.Component {
             <div className="flex justify-center items-center gap-2 ">
                 <h1 className="text-lg font-bold">{this.state.count}</h1>
                 <button
-                    className="p-2 bg-blue-300 rounded shadow-lg font-md"
+                    className="p-2 bg-slate-800 text-white rounded shadow-lg font-md"
                     onClick={() =>
                         this.setState({
                             count: this.state.count + 1,
@@ -22,6 +40,8 @@ class Counter extends React.Component {
                 >
                     +
                 </button>
+                {/*  #3 */}
+                {console.log('render')}
             </div>
         );
     }
